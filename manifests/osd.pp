284ee6aa60d5a6a9c578c5b3efcc5fcb95df1072
Atze de Vries <atze.devries@naturalis.nl>
2014-01-21 16:49:55 +0100

    added location to osd setting

--- manifests/osd.pp
+++ manifests/osd.pp
@@ -1,23 +1,21 @@
 class role_ceph::osd (
-	$fsid,
-	$monitorkey,
-	$adminkey,
-	$datacenter,
-	$rack,
-	$disks  = 'sdb,sdc,sdd'
-	){
-
-	class {"ceph::package":}
-	class {"ceph::osd":
-		monitorkey	=> $monitorkey,
-		adminkey	=> $adminkey,
-		fsid		=> $fsid,
-		datacenter	=> $datacenter,
-		room 		=> 'room',
-		row			=> 'row',
-		rack		=> $rack,
-		disks 		=> $disks,
-		require		=> Class['ceph::package'],
-	}
-
+  $fsid,
+  $monitorkey,
+  $adminkey,
+  $datacenter,
+  $rack,
+  $disks  = 'sdb,sdc,sdd',
+  ){
+  class {"ceph::package":}
+  class {"ceph::osd":
+    monitorkey	=> $monitorkey,
+    adminkey	=> $adminkey,
+    fsid		=> $fsid,
+    datacenter	=> $datacenter,
+    room 		=> 'room',
+    row			=> 'row',
+    rack		=> $rack,
+    disks 		=> $disks,
+    require		=> Class['ceph::package'],
+  }
 }
